fd50d008bbb527d96eaaa076e188dc29068520c6
gamechanger638 <84618431+gamechanger638@users.noreply.github.com>
2026-05-01 16:45:52 +0530

    fix db url

--- backend/settings.py
+++ backend/settings.py
@@ -86,9 +86,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
